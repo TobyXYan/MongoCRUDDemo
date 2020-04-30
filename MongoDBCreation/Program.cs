a8f3eed643441a725cbaca954a458f3b1d43e96f
Yan, Toby <Toby.Yan@kla-tencor.com>
2020-04-30 13:28:55 +0800

    Database & Collection Creation

---
+++ MongoDBCreation/Program.cs
@@ -0,0 +1,23 @@
+﻿using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MongoDBCreation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var mongoUrl = new MongoUrlBuilder("mongodb://localhost");
+            var dbClient = new MongoClient(mongoUrl.ToMongoUrl());
+            var dbNames = dbClient.ListDatabaseNames().ToList();
+            if(dbNames.Contains("People"))
+                return;
+            var database = dbClient.GetDatabase("People");
+            database.CreateCollectionAsync("Persons");
+            Console.ReadLine();
+        }
+    }
+}
+
